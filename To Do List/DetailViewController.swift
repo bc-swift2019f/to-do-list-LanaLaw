4c0aedba16be3ac3a264cedda5297f50b971ec1a
Ellana Lawrence <elawrence.blain@gmail.com>
2019-09-23 15:38:00 -0400

    5.5 Complete

--- To Do List/DetailViewController.swift
+++ To Do List/DetailViewController.swift
@@ -11,23 +11,39 @@ import UIKit
 class DetailViewController: UIViewController {
 
     @IBOutlet weak var toDoField: UITextField!
+    @IBOutlet weak var saveBarButton: UIBarButtonItem!
     var toDoItem: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         if let toDoItem = toDoItem{
             toDoField.text = toDoItem
         }
+        enableDisableButton()
+        toDoField.becomeFirstResponder()
+
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "UnwindFromSave" {
             toDoItem = toDoField.text
         }
+        
+    }
+    
+    func enableDisableButton(){
+        if let toDoFieldCount = toDoField.text?.count, toDoFieldCount > 0 {
+            saveBarButton.isEnabled = true
+        } else {
+            saveBarButton.isEnabled = false
+        }
     }
     
     
+    
+    @IBAction func toDoFieldChanged(_ sender: UITextField) {
+        enableDisableButton()
+    }
     
     @IBAction func cancelPressed(_ sender: UIBarButtonItem) {
         let isPresentingInAddMode = presentingViewController is UINavigationController
