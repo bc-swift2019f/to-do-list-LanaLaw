2b9801e61593691f863b4dcc00bb86420e734c59
Ellana Lawrence <elawrence.blain@gmail.com>
2019-09-23 09:48:27 -0400

    5.3 Complete

--- To Do List/DetailViewController.swift
+++ To Do List/DetailViewController.swift
@@ -11,14 +11,27 @@ import UIKit
 class DetailViewController: UIViewController {
 
     @IBOutlet weak var toDoField: UITextField!
+    var toDoItem: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        if let toDoItem = toDoItem{
+            toDoField.text = toDoItem
+        }
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "UnwindFromSave" {
+            toDoItem = toDoField.text
+        }
+    }
+    
+    
     
     @IBAction func cancelPressed(_ sender: UIBarButtonItem) {
         let isPresentingInAddMode = presentingViewController is UINavigationController
-        if isPresentingInAddMode == true {
+        if isPresentingInAddMode{
             dismiss(animated: true, completion: nil)
         } else {
             navigationController?.popViewController(animated: true)
